612020daee32f828563b3ede9de7efe745455fcc
myraiden <klj3604@gmail.com>
2024-07-16 01:04:15 -0400

    all functions working and added

--- some-every.js
+++ some-every.js
@@ -6,7 +6,12 @@ Examples:
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+    return arr.some(function(oddValue,oddIndex,oddArray){
+        return (oddValue%2===1);
+    })
+    
+}
 
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
@@ -16,7 +21,17 @@ Examples:
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+    //the following code for mapping from Number to String to Array was found at https://www.geeksforgeeks.org/how-to-convert-a-number-into-array-in-javascript/
+    let numArray=String(num).split("").map(function(num){
+        return Number(num);
+    })
+
+    //student generated code resumes
+    return numArray.some(function(zeroValue,zeroIndex,zeroArray){
+        return (zeroValue===0);
+    })
+}
 
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
@@ -26,7 +41,11 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(oddVal, oddInd, oddArr){
+        return (oddVal%2===1);
+    })
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -36,7 +55,15 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(firstVal,firstIndex,firstArr){
+        let arr2=arr.toSpliced(firstIndex,1);
+        return arr2.every(function(compVal,compIndex,compArray){
+            return (firstVal!=compArray[compIndex]);
+        })
+    })
+   
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
@@ -53,7 +80,11 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(function(certainValue,certainIndex,certainArray){
+        return (certainValue[key]);
+    })
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -71,4 +102,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(allValue,allIndex,allArray){
+        return(allValue[key]===searchValue);
+    })
+}
